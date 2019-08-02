3eb5179b7c1161198a8512db571bc66ffd54f82e
Kamil Breguła <kamil.bregula@polidea.com>
2019-08-02 22:25:04 +0200

    Fix issues with muttable default value

--- airflow_munchkin/block_generator/blocks.py
+++ airflow_munchkin/block_generator/blocks.py
@@ -1,7 +1,25 @@
 # -*- coding: utf-8 -*-
-from typing import NamedTuple, List, Dict, Any, Set, Optional
+from typing import Any, Dict, List, NamedTuple, Optional, Set
 
 from airflow_munchkin.client_parser.docstring_parser.bricks import TypeBrick
+
+
+DEFAULT_IMPORTS = (
+    "typing.Union",
+    "typing.Optional",
+    "typing.Sequence",
+    "typing.Tuple",
+    "typing.Dict",
+    "airflow.AirflowException",
+    "airflow.contrib.hooks.gcp_api_base_hook.GoogleCloudBaseHook",
+    "google.api_core.retry.Retry",
+    "google.cloud.redis_v1beta1.CloudRedisClient",
+    "tests.contrib.utils.base_gcp_mock.mock_base_gcp_hook_default_project_id",
+    "tests.contrib.utils.base_gcp_mock.mock_base_gcp_hook_no_default_project_id",
+    "tests.contrib.utils.base_gcp_mock.GCP_PROJECT_ID_HOOK_UNIT_TEST",
+    "unittest.TestCase",
+    "unittest.mock",
+)
 
 
 class CodeBlock(NamedTuple):
@@ -38,23 +56,34 @@ class Constant(NamedTuple):
     kind: Optional[TypeBrick] = None
 
 
-class FileBlock(NamedTuple):
-    file_name: str
-    class_blocks: List[ClassBlock]
-    import_statement: Set[str] = {
-        "typing.Union",
-        "typing.Optional",
-        "typing.Sequence",
-        "typing.Tuple",
-        "typing.Dict",
-        "airflow.AirflowException",
-        "airflow.contrib.hooks.gcp_api_base_hook.GoogleCloudBaseHook",
-        "google.api_core.retry.Retry",
-        "google.cloud.redis_v1beta1.CloudRedisClient",
-        "tests.contrib.utils.base_gcp_mock.mock_base_gcp_hook_default_project_id",
-        "tests.contrib.utils.base_gcp_mock.mock_base_gcp_hook_no_default_project_id",
-        "tests.contrib.utils.base_gcp_mock.GCP_PROJECT_ID_HOOK_UNIT_TEST",
-        "unittest.TestCase",
-        "unittest.mock",
-    }
-    constants: List[Constant] = []
+class FileBlock:
+    def __init__(
+        self,
+        file_name: str,
+        class_blocks: List[ClassBlock],
+        import_statement: Set[str] = None,
+        constants: List[Constant] = None,
+    ):
+        self.file_name = file_name
+        self.class_blocks = class_blocks
+        self.import_statement = import_statement or {*DEFAULT_IMPORTS}
+        self.constants = constants or []
+
+    def __str__(self) -> str:
+        return (
+            f'FileBlock(file_name={repr(self.file_name)}", class_blocks={self.class_blocks}, '
+            f"import_statement={self.import_statement}, constants={repr(self.constants)})"
+        )
+
+    def __eq__(self, other):
+        if isinstance(other, self.__class__):
+            return self.__dict__ == other.__dict__
+        return False
+
+    def __repr__(self) -> str:
+        return str(self)
+
+    def __hash__(self):
+        return hash(
+            (self.file_name, self.class_blocks, self.import_statement, self.constants)
+        )
--- tests/block_generator/test_operator_generator.py
+++ tests/block_generator/test_operator_generator.py
@@ -5,7 +5,7 @@ from airflow_munchkin.block_generator import operator_generator, FileBlock
 from airflow_munchkin.integration import Integration
 
 
-class TestcreateFileBlock(TestCase):
+class TestCreateFileBlock(TestCase):
     def test_create_file_block(self):
         integration = Integration(
             service_name="SERVICE_NAME",
@@ -19,20 +19,20 @@ class TestcreateFileBlock(TestCase):
                 file_name="FILE_PREFFIX_operator.py",
                 class_blocks=[],
                 import_statement={
-                    "tests.contrib.utils.base_gcp_mock.mock_base_gcp_hook_default_project_id",
-                    "tests.contrib.utils.base_gcp_mock.mock_base_gcp_hook_no_default_project_id",
+                    "typing.Sequence",
                     "unittest.TestCase",
                     "typing.Union",
-                    "airflow.contrib.hooks.gcp_api_base_hook.GoogleCloudBaseHook",
-                    "unittest.mock",
-                    "typing.Optional",
-                    "google.cloud.redis_v1beta1.CloudRedisClient",
+                    "tests.contrib.utils.base_gcp_mock.mock_base_gcp_hook_default_project_id",
                     "tests.contrib.utils.base_gcp_mock.GCP_PROJECT_ID_HOOK_UNIT_TEST",
-                    "typing.Sequence",
-                    "typing.Dict",
                     "airflow.AirflowException",
-                    "google.api_core.retry.Retry",
+                    "google.cloud.redis_v1beta1.CloudRedisClient",
+                    "tests.contrib.utils.base_gcp_mock.mock_base_gcp_hook_no_default_project_id",
+                    "typing.Optional",
+                    "unittest.mock",
                     "typing.Tuple",
+                    "airflow.contrib.hooks.gcp_api_base_hook.GoogleCloudBaseHook",
+                    "typing.Dict",
+                    "google.api_core.retry.Retry",
                 },
                 constants=[],
             ),
